cd74b23073ce28cd2f14a1f963a43a713bfb6a1e
MONTEL SACHA p1908066 <sacha.montel@etu.univ-lyon1.fr>
2021-12-19 19:45:17 +0100

    ajout des getter et des setter

--- src/Personne.java
+++ src/Personne.java
@@ -4,7 +4,7 @@ public abstract class Personne {
 	private String nom;
 	private String prenom;
 	private Float[] opinions;
-	
+
 	public Personne(String n, String p, Float[] o){
 		id = nombrePersonne;
 		nombrePersonne++;
@@ -12,4 +12,38 @@ public abstract class Personne {
 		prenom = p;
 		opinions = o;
 	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return this.nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return this.prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Float[] getOpinions() {
+		return this.opinions;
+	}
+
+	public void setOpinions(Float[] opinions) {
+		this.opinions = opinions;
+	}
+	
+	
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -3,23 +3,46 @@ public class Simulation {
 	private Electeur[] electeurs;
 	private Candidat[] candidats;
 
-
-	public Simulation(Scrutin s, Electeur[] e, Candidat[] c){
-		scrutin=s;
-		electeurs=e;
-		candidats=c;
+	public Simulation(Scrutin s, Electeur[] e, Candidat[] c) {
+		scrutin = s;
+		electeurs = e;
+		candidats = c;
 	}
 
-	public void sondage(){
+	public Scrutin getScrutin() {
+		return this.scrutin;
+	}
+
+	public void setScrutin(Scrutin scrutin) {
+		this.scrutin = scrutin;
+	}
+
+	public Electeur[] getElecteurs() {
+		return this.electeurs;
+	}
+
+	public void setElecteurs(Electeur[] electeurs) {
+		this.electeurs = electeurs;
+	}
+
+	public Candidat[] getCandidats() {
+		return this.candidats;
+	}
+
+	public void setCandidats(Candidat[] candidats) {
+		this.candidats = candidats;
+	}
+
+	public void sondage() {
 
 	}
 
-	public void evolution(TypeEvolution te){
+	public void evolution(TypeEvolution te) {
 
 	}
 
-	public void election(){
-		
+	public void election() {
+
 	}
-	
+
 }
