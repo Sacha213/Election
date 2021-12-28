4d85c88e237f584d5432283a482242525ba69e81
Sacha MONTEL <sacha.montel@univ-lyon1.fr>
2021-12-28 19:01:29 +0100

    modification mineure

--- src/main/java/com/mycompany/election/App.java
+++ src/main/java/com/mycompany/election/App.java
@@ -25,78 +25,74 @@ public class App {
 
             choix = scanner.nextInt();
 
-            if (choix == 1) {
-                // Initialisation
-
-                try {
-                    File file = new File("Election.csv");
-                    FileReader filereader = new FileReader(file);
-                    CSVReader csvReader = new CSVReader(filereader);
-
-                    List<String[]> allData = csvReader.readAll();
-
-                    allData.remove(0);
-                    Electeur[] electeurs = new Electeur[allData.size()-1];
-                    for (int j=0;j<allData.size()-1; j++) {
-                        String name = allData.get(j)[0];
-                        Float[] opinions = new Float[allData.get(j).length-1];
-                        for(int i=1;i<allData.get(j).length;i++){
-                            opinions[i-1]=Float.valueOf(allData.get(j)[i]);
-                        }
-                        Electeur electeur = new Electeur(name, null,opinions);
-                        electeurs[j]=electeur;
-                    }
-                    simulation.setElecteurs(electeurs);
-                } catch (Exception e) {
-                    System.out.println(e);
-                    System.out.println("Une erreur est survenue");
-                }
-
-            } else if (choix == 2) {
-                // Choisir un scrutin
-                System.out.println("Taper 1 pour : Majoritaire à un tour");
-                System.out.println("Taper 2 pour : Majoritaire à deux tours");
-                System.out.println("Taper 3 pour : Approbation");
-                System.out.println("Taper 4 pour : Alternatif");
-                System.out.println("Taper 5 pour : Borda");
-
-                choix = scanner.nextInt();
-
-                Scrutin scrutin = null;
-
-                if (choix==1) {
-                    scrutin = new Majoritaire1t();
+            switch (choix) {
+                case 1:
+                    // Initialisation
                     
-                }
-                else if(choix==2){
-                    scrutin = new Majoritaire2t();
-
-                }
-                else if(choix==3){
-                    scrutin = new Approbation();
-                }
-                else if(choix==4){
-                    scrutin = new Alternatif();
-                }
-                else if(choix==5){
-                    scrutin = new Borda();
-                }
-                else {
+                    try {
+                        File file = new File("Election.csv");
+                        FileReader filereader = new FileReader(file);
+                        CSVReader csvReader = new CSVReader(filereader);
+                        
+                        List<String[]> allData = csvReader.readAll();
+                        
+                        allData.remove(0);
+                        Electeur[] electeurs = new Electeur[allData.size()-1];
+                        for (int j=0;j<allData.size()-1; j++) {
+                            String name = allData.get(j)[0];
+                            Float[] opinions = new Float[allData.get(j).length-1];
+                            for(int i=1;i<allData.get(j).length;i++){
+                                opinions[i-1]=Float.valueOf(allData.get(j)[i]);
+                            }
+                            Electeur electeur = new Electeur(name, null,opinions);
+                            electeurs[j]=electeur;
+                        }
+                        simulation.setElecteurs(electeurs);
+                    } catch (Exception e) {
+                        System.out.println(e);
+                        System.out.println("Une erreur est survenue");
+                    }   break;
+                case 2:
+                    // Choisir un scrutin
+                    System.out.println("Taper 1 pour : Majoritaire à un tour");
+                    System.out.println("Taper 2 pour : Majoritaire à deux tours");
+                    System.out.println("Taper 3 pour : Approbation");
+                    System.out.println("Taper 4 pour : Alternatif");
+                    System.out.println("Taper 5 pour : Borda");
+                    choix = scanner.nextInt();
+                    Scrutin scrutin = null;
+                    switch (choix) {
+                        case 1:
+                            scrutin = new Majoritaire1t();
+                            break;
+                        case 2:
+                            scrutin = new Majoritaire2t();
+                            break;
+                        case 3:
+                            scrutin = new Approbation();
+                            break;
+                        case 4:
+                            scrutin = new Alternatif();
+                            break;
+                        case 5:
+                            scrutin = new Borda();
+                            break;
+                        default:
+                            System.out.println("Oups, veuillez entrer une valeur correcte");
+                            break;
+                    }   simulation.setScrutin(scrutin);
+                    break;
+            // Effectuer une simulation
+                case 3:
+                    break;
+                case 4:
+                    // Quitter
+                    System.out.println("Au revoir");
+                    continuer = false;
+                    break;
+                default:
                     System.out.println("Oups, veuillez entrer une valeur correcte");
-                }
-
-                simulation.setScrutin(scrutin);
-
-            } else if (choix == 3) {
-                // Effectuer une simulation
-
-            } else if (choix == 4) {
-                // Quitter
-                System.out.println("Au revoir");
-                continuer = false;
-            } else {
-                System.out.println("Oups, veuillez entrer une valeur correcte");
-                
+                    break;
             }
 
         }
